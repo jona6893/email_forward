444a478cd03610dba648804c786c148bf06d5292
”Jonathan <jona6893@stud.kea.dkgit config --global user.name Jonathan Weldongit config --global user.email jona6893@stud.kea.dk>
2023-05-30 14:02:18 +0200

    email forward server

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+const cors = require('cors');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post('/send', async (req, res) => {
+    const {email} = req.body;
+    
+    let transporter = nodemailer.createTransport({
+        service: 'gmail', // Replace this with your email service
+        auth: {
+            user: 'your-email@gmail.com', // Replace this with your email
+            pass: 'your-password' // Replace this with your password
+        }
+    });
+
+    let mailOptions = {
+        from: 'your-email@gmail.com', // Replace this with your email
+        to: email, // to the email from the request body
+        subject: 'Hello from Node.js Server',
+        text: 'This email was sent from Node.js server'
+    };
+
+    try {
+        await transporter.sendMail(mailOptions);
+        res.status(200).json({message: 'Email sent successfully'});
+    } catch (error) {
+        res.status(500).json({error: 'There was an error sending the email'});
+    }
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
